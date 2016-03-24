64c34558c3b92009dd7402ba8e3673b8fc44c560
SingleXL <2217232293@qq.com>
2016-03-24 15:28:35 +0800

    add

--- lucene/src/main/java/com/study/lucene/IndexUtil.java
+++ lucene/src/main/java/com/study/lucene/IndexUtil.java
@@ -1,12 +1,67 @@
 package com.study.lucene;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.index.CorruptIndexException;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexReader.FieldOption;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.LockObtainFailedException;
+import org.apache.lucene.util.Version;
+
 public class IndexUtil {
 
 	private String[] ids = { "1", "2", "3", "4", "5", "6" };
 	private String[] emails = { "aa@itat.org", "bb@itat.org", "cc@itat.org", "dd@itat.org", "ee@itat.org", "ff@itat.org" };
 	private String[] content = { "welcome to visited the space", "hello", "my name is cc", "like football", "dasda", "dqwww" };
-	private int[] attachs = {2,1,3,4,2,4};
-	private String[] names = {"zhangsan","lisi","yuee","yetty","mike","tom"};
-	
+	private int[] attachs = { 2, 1, 3, 4, 2, 4 };
+	private String[] names = { "zhangsan", "lisi", "yuee", "yetty", "mike", "tom" };
+	private Directory directory = null;
+
+	public IndexUtil() {
+		try {
+			directory = FSDirectory.open(new File("D:/lucene_index/index02"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void index() {
+		IndexWriter writer = null;
+
+		try {
+			writer = new IndexWriter(directory, new IndexWriterConfig(Version.LUCENE_35, new StandardAnalyzer(Version.LUCENE_35)));
+			
+			Document doc = null;
+			for (int i = 0; i < ids.length; i++) {
+				
+				doc.add(new Field("id",ids[i],Field.Store.YES,Field.Index.ANALYZED));
+				
+			}
+			
+		} catch (CorruptIndexException e) {
+			e.printStackTrace();
+		} catch (LockObtainFailedException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (writer != null)
+					writer.close();
+			} catch (CorruptIndexException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
 
 }
