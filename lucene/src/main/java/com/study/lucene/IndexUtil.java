daadf6f62c5114b7a009ecdef38a75c7d3c3b4a7
SingleXL <2217232293@qq.com>
2016-03-24 14:40:07 +0800

    indexutil

--- lucene/src/main/java/com/study/lucene/IndexUtil.java
+++ lucene/src/main/java/com/study/lucene/IndexUtil.java
@@ -1,7 +1,12 @@
 package com.study.lucene;
 
 public class IndexUtil {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
+
+	private String[] ids = { "1", "2", "3", "4", "5", "6" };
+	private String[] emails = { "aa@itat.org", "bb@itat.org", "cc@itat.org", "dd@itat.org", "ee@itat.org", "ff@itat.org" };
+	private String[] content = { "welcome to visited the space", "hello", "my name is cc", "like football", "dasda", "dqwww" };
+	private int[] attachs = {2,1,3,4,2,4};
+	private String[] names = {"zhangsan","lisi","yuee","yetty","mike","tom"};
+	
+
 }
